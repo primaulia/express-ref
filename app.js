cc12c61e7ef1beafdd65b771aa4632a1ec0350d5
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-11 14:37:18 +0800

    routes update for posts

--- app.js
+++ app.js
@@ -5,34 +5,20 @@ var express = require('express')
 var app = express()
 var port = 4000
 
-app.use(function (req, res, next) {
-  console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
-  next()
-})
+// app.use(function (req, res, next) {
+//   console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
+//   next()
+// })
 
-// set the require routes
+// set all the routes
 var routes = require('./routes/index')
 var user_routes = require('./routes/users')
+var posts_routes = require('./routes/posts')
 
 // set the middleware for routes
 app.use('/', routes)
 app.use('/users', user_routes)
-
-// take this out from app.js
-
-// setting the route to homepage
-// app.get('/path-name', callback(request, response))
-// app.get('/', function (req, res) {
-//   res.send('hello world, this is the homepage')
-// })
-//
-// app.get('/about', function (req, res) {
-//   res.send('this is the about page')
-// })
-//
-// app.get('/contact', function (req, res) {
-//   res.send('this is the contact page')
-// })
+app.use('/posts', post_routes)
 
 // listening to the opened port
 app.listen(port)
---
+++ routes/posts.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var router = express.Router()
+
+router.get('/posts', function (req, res) {
+  res.send('this is the posts homepage')
+})
+
+router.get('/posts/new', function (req, res) {
+  res.send('NEW NEW NEW HERE')
+})
+
+router.get('/posts/:id', function (req, res) {
+  console.log('this is the object of req params: ' + req.params)
+  res.send('this is posts ' + req.params.id + ' homepage')
+})
+
+router.get('/posts/:id/edit', function (req, res) {
+  res.send('this is posts 1 edit page')
+})
+
+module.exports = router
