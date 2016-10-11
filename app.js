b38822c2c6236b939bd3972f68f1adde35047713
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-11 15:43:29 +0800

    initial implementation of the views

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var express = require('express')
 var app = express()
 var port = 4000
 
+// require path
+var path = require('path')
+
+// running express with ejs
+app.set('view engine', 'ejs')
+
+// app.use('/', express.static(path.join(__dirname, 'views')))
+
 // example of a middleware
 // app.use(function (req, res, next) {
 //   console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
@@ -17,9 +25,14 @@ var user_routes = require('./routes/users')
 var posts_routes = require('./routes/posts')
 
 // set the middleware for routes
-app.use('/', routes)
-app.use('/users', user_routes)
-app.use('/posts', posts_routes)
+app.get('/', function (req, res) {
+  // res.send('homepage')
+  res.render('index', {name: 'Glen Choo'})
+})
+
+// app.use('/', routes)
+// app.use('/users', user_routes)
+// app.use('/posts', posts_routes)
 
 // listening to the opened port
 app.listen(port)
